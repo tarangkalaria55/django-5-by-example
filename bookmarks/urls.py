4aea478b0f91d5c1bfc90f85778b3b936fd45c3d
Tarng Kalariya <tarang.appliquer@gmail.com>
2025-02-07 10:05:45 +0530

    Add actions app with models, views, tests, and utilities; update requirements and templates

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
         include("social_django.urls", namespace="social"),
     ),
     path("images/", include("images.urls", namespace="images")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 if settings.DEBUG:
